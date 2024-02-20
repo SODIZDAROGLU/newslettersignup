95a2b4d3203c7f9b2e41fdb67bdac47d74cc455c
sodizdaroglu <americanlaz@gmail.com>
2024-02-20 08:40:30 -0500

    update

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require ("request");
- 
+const https = require('https');
+
+
 
 const app = express();
 
@@ -35,28 +37,65 @@ app.post("/", function(req,res){
             }
         ]
     };
+    
     //data turn into string
     const jsonData = JSON.stringify(data);
-    const url="https://us18.api.mailchimp.com/3.0/lists/c5109b66a9";
+    const url="https://us21.api.mailchimp.com/3.0/lists/89d565538e";
     const options = {
         method: "POST",
-        auth:"oz:ab2e8b7dd662b1ee5e2377c85d88479d-us18"
+        auth:"oz:334534b565887ff03ca08cb84035ffbf-us21",
+     
     }
-    const request = https.request(url, options, function(response){
+    const request = https.request(url, options, function (response) {
+        let responseData = "";
 
-           if( response.statusCode === 200){
-               res.sendFile(__dirname + "/success.html");
-           } else {
-               res.send(res.sendFile + "/failure.html");
-           }
-           response.on("data", function(data){
-               console.log(JSON.parse(data));
-           });
+        response.on("data", function (chunk) {
+            responseData += chunk;
+        });
+        //console.log(JSON.parse(responseData));
+        var err = false;
+        response.on("end", function () {
+            const parsedResponse = JSON.parse(responseData);
+            const errors = parsedResponse.errors;
+            errors.forEach(error => {
+                err = error.error;
+                console.log("Email Address:", error.email_address);
+                console.log("Error:", error.error);
+                console.log("Error Code:", error.error_code);
+            });
+            if (response.statusCode === 200 && !err) {
+                res.sendFile(__dirname + "/success.html");
+              
+            } else {
+                res.status(response.statusCode).sendFile(__dirname + "/failure.html");
+            }
+      
+           // console.log(JSON.parse(responseData));
+        });
     });
-    request.write(jsonData);
-    request.end();
-});
+
+    request.on("error", function (error) {
+        console.error("Error while making request to Mailchimp API:", error);
+        res.status(500).send("Failed to subscribe. Please try again later.");
+    });
+            
+    
+       request.write(jsonData);
+       request.end();
+    
+    });
+       
+
+     
+         
+            
+
+
+
 //api key
 //ab2e8b7dd662b1ee5e2377c85d88479d-us18
-
 //c5109b66a9
+
+
+
+
